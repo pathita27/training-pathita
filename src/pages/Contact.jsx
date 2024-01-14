b8fe833b8e395b90d98e17a29dedc62360d001db
pathita27 <pathita271@gmail.com>
2024-01-14 14:08:36 +0700

    first commit

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <>
+      <h1>ติดต่อ</h1><hr/>
+      <h2>NVC Training Center</h2>
+      <h4>Address: 90 Tesa Rd.,Mueang,Nakhon Pathom 73000</h4>
+      <h4>Tel:034-252790</h4>
+      <h4>E-mail: admin@nvctrainingcenter.com</h4>
+      
+    </>
+  );
+}
+
+
+export default Contact
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home(){
+ return(
+    <div>
+        <h1>หน้าหลัก</h1><hr/>
+        <center><img src="12.jpg"/></center>
+    </div>
+    
+
+ )
+}
+
+
+export default Home
